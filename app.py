22e4177610293dfc7319ae051cf1af5a1d8c2b8e
nisacodelifings <annisap.siswa@smkn4bdg.sch.id>
2020-08-23 21:20:22 -0700

    sip

--- app.py
+++ app.py
@@ -1,129 +1,159 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import messagebox
 from db import Database
 
+# Instanciate databse object
 db = Database('store.db')
 
-
-def populate_list():
-    parts_list.delete(0, END)
-    for row in db.fetch():
-        parts_list.insert(END, row)
+# Main Application/GUI class
 
 
-def add_item():
-    if part_text.get() == '' or customer_text.get() == '' or retailer_text.get() == '' or price_text.get() == '':
-        messagebox.showerror('Required Fields', 'Please include all fields')
-        return
-    db.insert(part_text.get(), customer_text.get(),
-              retailer_text.get(), price_text.get())
-    parts_list.delete(0, END)
-    parts_list.insert(END, (part_text.get(), customer_text.get(),
-                            retailer_text.get(), price_text.get()))
-    clear_text()
-    populate_list()
+class Application(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.master = master
+        master.title('Aplikasi Pengelolaan Stok Barang | nisacodelifings')
+        # Width height
+        master.geometry("700x350")
+        # Create widgets/grid
+        self.create_widgets()
+        # Init selected item var
+        self.selected_item = 0
+        # Populate initial list
+        self.populate_list()
+
+    def create_widgets(self):
+        # Part
+        self.part_text = tk.StringVar()
+        self.part_label = tk.Label(
+            self.master, text='Kode Barang', font=('bold', 14), pady=20)
+        self.part_label.grid(row=0, column=0, sticky=tk.W)
+        self.part_entry = tk.Entry(self.master, textvariable=self.part_text)
+        self.part_entry.grid(row=0, column=1)
+        # Customer
+        self.customer_text = tk.StringVar()
+        self.customer_label = tk.Label(
+            self.master, text='Nama Barang', font=('bold', 14))
+        self.customer_label.grid(row=0, column=2, sticky=tk.W)
+        self.customer_entry = tk.Entry(
+            self.master, textvariable=self.customer_text)
+        self.customer_entry.grid(row=0, column=3)
+        # Retailer
+        self.retailer_text = tk.StringVar()
+        self.retailer_label = tk.Label(
+            self.master, text='Harga Barang', font=('bold', 14))
+        self.retailer_label.grid(row=1, column=0, sticky=tk.W)
+        self.retailer_entry = tk.Entry(
+            self.master, textvariable=self.retailer_text)
+        self.retailer_entry.grid(row=1, column=1)
+        # Price
+        self.price_text = tk.StringVar()
+        self.price_label = tk.Label(
+            self.master, text='Stok Barang', font=('bold', 14))
+        self.price_label.grid(row=1, column=2, sticky=tk.W)
+        self.price_entry = tk.Entry(self.master, textvariable=self.price_text)
+        self.price_entry.grid(row=1, column=3)
+
+        # Parts list (listbox)
+        self.parts_list = tk.Listbox(self.master, height=8, width=50, border=0)
+        self.parts_list.grid(row=3, column=0, columnspan=3,
+                             rowspan=6, pady=20, padx=20)
+        # Create scrollbar
+        self.scrollbar = tk.Scrollbar(self.master)
+        self.scrollbar.grid(row=3, column=3)
+        # Set scrollbar to parts
+        self.parts_list.configure(yscrollcommand=self.scrollbar.set)
+        self.scrollbar.configure(command=self.parts_list.yview)
+
+        # Bind select
+        self.parts_list.bind('<<ListboxSelect>>', self.select_item)
+
+        # Buttons
+        self.add_btn = tk.Button(
+            self.master, text="Tambah", width=12, command=self.add_item)
+        self.add_btn.grid(row=2, column=0, pady=20)
+
+        self.remove_btn = tk.Button(
+            self.master, text="Hapus", width=12, command=self.remove_item)
+        self.remove_btn.grid(row=2, column=1)
+
+        self.update_btn = tk.Button(
+            self.master, text="Update", width=12, command=self.update_item)
+        self.update_btn.grid(row=2, column=2)
+
+        self.exit_btn = tk.Button(
+            self.master, text="Clear Input", width=12, command=self.clear_text)
+        self.exit_btn.grid(row=2, column=3)
+
+    def populate_list(self):
+        # Delete items before update. So when you keep pressing it doesnt keep getting (show example by calling this twice)
+        self.parts_list.delete(0, tk.END)
+        # Loop through records
+        for row in db.fetch():
+            # Insert into list
+            self.parts_list.insert(tk.END, row)
+
+    # Add new item
+    def add_item(self):
+        if self.part_text.get() == '' or self.customer_text.get() == '' or self.retailer_text.get() == '' or self.price_text.get() == '':
+            messagebox.showerror(
+                "Required Fields", "Please include all fields")
+            return
+        print(self.part_text.get())
+        # Insert into DB
+        db.insert(self.part_text.get(), self.customer_text.get(),
+                  self.retailer_text.get(), self.price_text.get())
+        # Clear list
+        self.parts_list.delete(0, tk.END)
+        # Insert into list
+        self.parts_list.insert(tk.END, (self.part_text.get(), self.customer_text.get(
+        ), self.retailer_text.get(), self.price_text.get()))
+        self.clear_text()
+        self.populate_list()
+
+    # Runs when item is selected
+    def select_item(self, event):
+        # # Create global selected item to use in other functions
+        # global self.selected_item
+        try:
+            # Get index
+            index = self.parts_list.curselection()[0]
+            # Get selected item
+            self.selected_item = self.parts_list.get(index)
+            # print(selected_item) # Print tuple
+
+            # Add text to entries
+            self.part_entry.delete(0, tk.END)
+            self.part_entry.insert(tk.END, self.selected_item[1])
+            self.customer_entry.delete(0, tk.END)
+            self.customer_entry.insert(tk.END, self.selected_item[2])
+            self.retailer_entry.delete(0, tk.END)
+            self.retailer_entry.insert(tk.END, self.selected_item[3])
+            self.price_entry.delete(0, tk.END)
+            self.price_entry.insert(tk.END, self.selected_item[4])
+        except IndexError:
+            pass
+
+    # Remove item
+    def remove_item(self):
+        db.remove(self.selected_item[0])
+        self.clear_text()
+        self.populate_list()
+
+    # Update item
+    def update_item(self):
+        db.update(self.selected_item[0], self.part_text.get(
+        ), self.customer_text.get(), self.retailer_text.get(), self.price_text.get())
+        self.populate_list()
+
+    # Clear all text fields
+    def clear_text(self):
+        self.part_entry.delete(0, tk.END)
+        self.customer_entry.delete(0, tk.END)
+        self.retailer_entry.delete(0, tk.END)
+        self.price_entry.delete(0, tk.END)
 
 
-def select_item(event):
-    try:
-        global selected_item
-        index = parts_list.curselection()[0]
-        selected_item = parts_list.get(index)
-
-        part_entry.delete(0, END)
-        part_entry.insert(END, selected_item[1])
-        customer_entry.delete(0, END)
-        customer_entry.insert(END, selected_item[2])
-        retailer_entry.delete(0, END)
-        retailer_entry.insert(END, selected_item[3])
-        price_entry.delete(0, END)
-        price_entry.insert(END, selected_item[4])
-    except IndexError:
-        pass
-
-
-def remove_item():
-    db.remove(selected_item[0])
-    clear_text()
-    populate_list()
-
-
-def update_item():
-    db.update(selected_item[0], part_text.get(), customer_text.get(),
-              retailer_text.get(), price_text.get())
-    populate_list()
-
-
-def clear_text():
-    part_entry.delete(0, END)
-    customer_entry.delete(0, END)
-    retailer_entry.delete(0, END)
-    price_entry.delete(0, END)
-
-
-# Create window object
-app = Tk()
-
-# Part
-part_text = StringVar()
-part_label = Label(app, text='Kode', font=('bold', 14), pady=20)
-part_label.grid(row=0, column=0, sticky=W)
-part_entry = Entry(app, textvariable=part_text)
-part_entry.grid(row=0, column=1)
-# Customer
-customer_text = StringVar()
-customer_label = Label(app, text='Nama', font=('bold', 14))
-customer_label.grid(row=0, column=2, sticky=W)
-customer_entry = Entry(app, textvariable=customer_text)
-customer_entry.grid(row=0, column=3)
-# Retailer
-retailer_text = StringVar()
-retailer_label = Label(app, text='Harga', font=('bold', 14))
-retailer_label.grid(row=1, column=0, sticky=W)
-retailer_entry = Entry(app, textvariable=retailer_text)
-retailer_entry.grid(row=1, column=1)
-# Price
-price_text = StringVar()
-price_label = Label(app, text='Stok', font=('bold', 14))
-price_label.grid(row=1, column=2, sticky=W)
-price_entry = Entry(app, textvariable=price_text)
-price_entry.grid(row=1, column=3)
-# Parts List (Listbox)
-parts_list = Listbox(app, height=8, width=50, border=0)
-parts_list.grid(row=3, column=0, columnspan=3, rowspan=6, pady=20, padx=20)
-# Create scrollbar
-scrollbar = Scrollbar(app)
-scrollbar.grid(row=3, column=3)
-# Set scroll to listbox
-parts_list.configure(yscrollcommand=scrollbar.set)
-scrollbar.configure(command=parts_list.yview)
-# Bind select
-parts_list.bind('<<ListboxSelect>>', select_item)
-
-# Buttons
-add_btn = Button(app, text='Tambah', width=12, command=add_item)
-add_btn.grid(row=2, column=0, pady=20)
-
-remove_btn = Button(app, text='Hapus', width=12, command=remove_item)
-remove_btn.grid(row=2, column=1)
-
-update_btn = Button(app, text='Update', width=12, command=update_item)
-update_btn.grid(row=2, column=2)
-
-clear_btn = Button(app, text='Clear Input', width=12, command=clear_text)
-clear_btn.grid(row=2, column=3)
-
-app.title('Aplikasi Pengelolaan Stok Barang | nisacodelifings')
-app.geometry('700x350')
-
-# Populate data
-populate_list()
-
-# Start program
+root = tk.Tk()
+app = Application(master=root)
 app.mainloop()
-
-
-# To create an executable, install pyinstaller and run
-# '''
-# pyinstaller --onefile --add-binary='/System/Library/Frameworks/Tk.framework/Tk':'tk' --add-binary='/System/Library/Frameworks/Tcl.framework/Tcl':'tcl' part_manager.py
-# '''
